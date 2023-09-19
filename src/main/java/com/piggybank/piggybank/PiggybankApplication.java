7c80fd17353051bd319d0a6c68f1f1c77a9e21a2
Tushar Tiwaskar <tushartiwaskar.svpcet@gmail.com>
2023-09-19 20:54:46 +0530

    v1

---
+++ src/main/java/com/piggybank/piggybank/PiggybankApplication.java
@@ -0,0 +1,17 @@
+package com.piggybank.piggybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggybankApplication.class, args);
+		
+	}
+
+}
+
+///piggybank-0.0.1-SNAPSHOT
+///C:\Users\Asus\Desktop\piggybank\https---github.com-tushartiwaskar1997-PiggyBank\piggybank>mvnw.cmd clean package
